7a5e35a63cb406be393f3462bc6432822293542f
Lisandro Agustín Fernandez Brandt <89790156+Lisandro939@users.noreply.github.com>
2023-10-28 12:41:58 -0300

    Update AboutUs.tsx

--- src/components/AboutUs/AboutUs.tsx
+++ src/components/AboutUs/AboutUs.tsx
@@ -6,13 +6,10 @@ const AboutUs = () => {
         <div>
         <h2>Sobre nosotros</h2>
         <p>
-        EspiSport es el lugar perfecto para los apasionados seguidores del Club Atlético Boca Juniors que desean mostrar su orgullo por uno de los equipos de fútbol más icónicos de Argentina y del mundo. Esta tienda de ropa deportiva se encuentra estratégicamente ubicada cerca del estadio La Bombonera, en el corazón de Buenos Aires, convirtiéndola en una parada obligatoria para los hinchas que buscan las últimas novedades en indumentaria y accesorios relacionados con el club.
+            Boca Sports
         </p>
         <p>
         Nuestra tienda ofrece una amplia gama de camisetas, pantalones cortos y medias, todos con los colores característicos del club, azul y amarillo. Los fans pueden encontrar tanto las camisetas de juego utilizadas por los jugadores como las réplicas para los seguidores. También hay opciones para hombres, mujeres y niños.
-        </p>
-        <p>
-          No dude en contactarnos en espisport@gmail.com para cualquier consulta o comentario.
         </p>
       </div>
       <div className="container">
